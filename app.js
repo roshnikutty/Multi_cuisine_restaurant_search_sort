fea1c7970b9abc3dd874d68fa2c725d6de52bff7
Roshni Kutty <roshni.ajkutty@gmail.com>
2017-07-14 14:08:09 -0400

    scrolling map

--- app.js
+++ app.js
@@ -161,3 +161,25 @@ function getSearchParams() {
 
     return { inputCusineProp: cuisineSelection, inputRadiusProp: inputRadius };
 }
+
+//--------------------------------------- Map scroll------------------------------
+function mapScroll() {
+    var element = $('.follow-scroll'),
+        originalY = element.offset().top;
+    
+    // Space between element and top of screen (when scrolling)
+    var topMargin = -50;
+    element.css('position', 'relative');
+    
+    $(window).on('scroll', function(event) {
+        var scrollTop = $(window).scrollTop();
+        
+        element.stop(false, false).animate({
+            top: scrollTop < originalY
+                    ? 0
+                    : scrollTop - originalY + topMargin
+        }, 300);
+    });
+};
+
+$(mapScroll);
